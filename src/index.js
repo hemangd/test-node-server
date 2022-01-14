dd6cb3f91bc4dcf90bff09009b6dfad07252992d
Heman G <hemangd@users.noreply.github.com>
2022-01-15 09:31:42 +1000

    update port

--- src/index.js
+++ src/index.js
@@ -1,11 +1,10 @@
 const express = require("express");
 const app = express();
-const port = 3000;
 
 app.get("/", (req, res) => {
   res.json({ success: true });
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+app.listen(process.env.PORT || 3000, () => {
+  console.log(`Example app listening`);
 }); //the server object listens on port 8080
